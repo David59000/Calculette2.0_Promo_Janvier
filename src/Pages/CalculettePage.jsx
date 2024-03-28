a96be27ffcf57ddf4cff7a1c03ba408875b7d9d2
David <ricouarddavid@orange.fr>
2024-03-28 17:01:54 +0100

    ajout fonction Delete et Reset

--- src/Pages/CalculettePage.jsx
+++ src/Pages/CalculettePage.jsx
@@ -4,71 +4,216 @@ const Calculette = () => {
   // Etats - States
   // Créer un état total
   const [total, setTotal] = useState("");
-  
+  let reg = new RegExp("^[\\d\\W]+$");
+
   // Créer un état calcul
   const clickOnButton = (e) => {
     console.log(e.target.innerHTML);
-    setTotal(total + e.target.innerHTML)
-  }
-
-  const calc = () => {
-    if (total !=="")
-    setTotal = eval(total);
+    setTotal(total + e.target.innerHTML);
   };
 
+  const calc = () => {
+    if (total !== "") {
+      setTotal(eval(total));
+    }
+    }
+    const resetTotal =() => {
+        setTotal("");
+    }
+    const deleteTotal =() => {
+        setTotal(total.slice(0,-1));
+    }
+
+//   reset.addEventListener("click", () => {
+//     result.value = "";
+//   });
+//   deleteC.addEventListener("click", () => {
+//     result.value = result.value.slice(0, -1);
+//   });
 
   // Comportements - Les fonctions
 
   useEffect(() => {
+    document.addEventListener("keyup", (e) => {
+        console.log(e);
+        switch (e.key) {
+          case "Enter":
+            calc(total);
+            break;
+          case "Backspace":
+            deleteTotal();
+            break;
+          case "Delete":
+            resetTotal = "";
+            break;
+          default:
+            if (reg.test(e.key)) {
+              setTotal(total + e.key);
+            }
+            break;
+        }
+      });
+    
   }, []);
 
   // Affichages - return
 
   return (
     <>
-        
-        {/* html de la calculatrice */}
-        <div className="container">
-            <h1 className="text-center">Calculette avec REACT</h1>
-                <div className="col-6 offset-3 d-flex align-tems-center">
-                    <div className="input-group mb-3">
-                        <span className="input-group-text" id="totalCalculette">Total</span>
-                        <input type="text" className="form-control col-5" placeholder="Total" aria-label="Total" aria-describedby="totalCalculette" id="result"disabled value={total} />
-                    </div>
-                </div>
-        </div>            
-            
-                    <div className="d-flex flex-column align-items-center bg-secondary gap-3">
-                        <div className="d-flex flex-row gap-2 col-5">
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>1</button>
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>2</button>
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>3</button>
-                            <button type="button" className="btn btn-info col-3 caracterToAdd" onClick={clickOnButton}>+</button>
-                        </div>
-                        <div className="d-flex flex-row gap-2 col-5">
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>4</button>
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>5</button>
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>6</button>
-                            <button type="button" className="btn btn-info col-3 caracterToAdd" onClick={clickOnButton}>-</button>
-                        </div>
-                        <div className="d-flex flex-row gap-2 col-5">
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>7</button>
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>8</button>
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>9</button>
-                            <button type="button" className="btn btn-info col-3 caracterToAdd"    onClick={clickOnButton}>/</button>
-                        </div>
-                        <div className="d-flex flex-row gap-2 col-5">
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>.</button>
-                            <button type="button" className="btn btn-primary col-3 caracterToAdd" onClick={clickOnButton}>0</button>
-                            <button type="button" className="btn btn-danger col-3" id="reset">C</button>
-                            <button type="button" className="btn btn-info col-3 caracterToAdd" onClick={clickOnButton}>*</button>
-                        </div>
-                        <div className="d-flex flex-row gap-4 col-5">
-                            <button type="button" className="btn btn-danger col-9" id="deleteC">DELETE</button>
-                            <button type="button" className="btn btn-info col-3" id="egal">=</button>
-                        </div>
-                    </div>   
+      {/* html de la calculatrice */}
+      <div className="container">
+        <h1 className="text-center">Calculette avec REACT</h1>
+        <div className="col-6 offset-3 d-flex align-tems-center">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="totalCalculette">
+              Total
+            </span>
+            <input
+              type="text"
+              className="form-control col-5"
+              placeholder="Total"
+              aria-label="Total"
+              aria-describedby="totalCalculette"
+              id="result"
+              disabled
+              value={total}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="d-flex flex-column align-items-center bg-secondary gap-3">
+        <div className="d-flex flex-row gap-2 col-5">
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            1
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            2
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            3
+          </button>
+          <button
+            type="button"
+            className="btn btn-info col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            +
+          </button>
+        </div>
+        <div className="d-flex flex-row gap-2 col-5">
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            4
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            5
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            6
+          </button>
+          <button
+            type="button"
+            className="btn btn-info col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            -
+          </button>
+        </div>
+        <div className="d-flex flex-row gap-2 col-5">
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            7
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            8
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            9
+          </button>
+          <button
+            type="button"
+            className="btn btn-info col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            /
+          </button>
+        </div>
+        <div className="d-flex flex-row gap-2 col-5">
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            .
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            0
+          </button>
+          <button type="button" className="btn btn-danger col-3" id="reset" onClick={resetTotal}>
+            C
+          </button>
+          <button
+            type="button"
+            className="btn btn-info col-3 caracterToAdd"
+            onClick={clickOnButton}
+          >
+            *
+          </button>
+        </div>
+        <div className="d-flex flex-row gap-4 col-5">
+          <button type="button" className="btn btn-danger col-9" id="deleteC" onClick={deleteTotal}>
+            DELETE
+          </button>
+          <button
+            type="button"
+            className="btn btn-info col-3"
+            id="egal"
+            onClick={calc}
+          >
+            =
+          </button>
+        </div>
+      </div>
     </>
-    );
+  );
 };
 export default Calculette;
